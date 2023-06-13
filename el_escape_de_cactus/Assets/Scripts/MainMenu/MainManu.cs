3fdee197d4d77251e6e12fd6a6f4ca8d7eed6671
genihe <genihe@hotmail.com>
2023-06-13 18:35:22 -0300

    Se creó la escena Main Menu y codifico el cambio de escena

---
+++ el_escape_de_cactus/Assets/Scripts/MainMenu/MainManu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainManu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int sceneToLoad; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
