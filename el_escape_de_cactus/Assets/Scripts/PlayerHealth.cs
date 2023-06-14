1e15959ee17bf36897cfd87c2d793f88d9a9f2ec
Samanta Sein <savase88@yahoo.com.ar>
2023-06-13 21:05:14 -0300

    Fix: Life can't be lower than zero

--- el_escape_de_cactus/Assets/Scripts/PlayerHealth.cs
+++ el_escape_de_cactus/Assets/Scripts/PlayerHealth.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHealth : MonoBehaviour , IDamageable
+public class PlayerHealth : MonoBehaviour, IDamageable
 {
     // Start is called before the first frame update
     public static PlayerHealth instance;
+
     //public Image healthBar;
     public Text healthBar;
     public int currentHealth;
-    [SerializeField] int maxHealth=3;
 
-    private void Awake(){
-        instance=this;
+    [SerializeField]
+    int maxHealth = 3;
+
+    private void Awake()
+    {
+        instance = this;
     }
 
     void Start()
     {
         //Asigno la vida maxima a la vida de inicio
-        currentHealth=maxHealth;
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -28,12 +32,13 @@ public class PlayerHealth : MonoBehaviour , IDamageable
         //healthBar.fillAmount=GetPercentage();
 
         //Actualizo el Text en pantalla al valor de la vida
-        healthBar.text=GetLife().ToString();
-    }
-    public int GetLife(){
-        return currentHealth;
+        healthBar.text = GetLife().ToString();
     }
 
+    public int GetLife()
+    {
+        return currentHealth;
+    }
 
     //Problemas al incorporar el paquete 2D Sprite (postergar desarrollo)
     /*public float GetPercentage(){
@@ -44,22 +49,25 @@ public class PlayerHealth : MonoBehaviour , IDamageable
         currentHealth=Mathf.Min(currentHealth + amount, maxHealth);
     }*/
 
-    public void TakeDamage (int amount){
+    public void TakeDamage(int amount)
+    {
         //currentHealth=Mathf.Max(currentHealth - amount, 0.0f);
-        currentHealth-=amount;
-        if (currentHealth<=0)
+        currentHealth = currentHealth - amount >= 0 ? currentHealth - amount : 0;
+        if (currentHealth <= 0)
         {
             Defeat();
         }
     }
 
-    public void Defeat(){
+    public void Defeat()
+    {
         //currentHealth=maxHealth;
         Debug.Log("Estoy derrotado  :'-(");
         //reload the SCENE
     }
 }
 
-public interface IDamageable{
+public interface IDamageable
+{
     void TakeDamage(int amount);
 }
