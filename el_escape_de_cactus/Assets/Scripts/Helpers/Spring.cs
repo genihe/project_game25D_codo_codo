4f299bea0cee876748f608e0be1fe78e8c2892c0
Nicolás Flandua <nicolas.flandua@gmail.com>
2023-07-03 19:27:49 -0300

    música de fondo y sfx
    Se agrega música de fondo y sfx de rebote

--- el_escape_de_cactus/Assets/Scripts/Helpers/Spring.cs
+++ el_escape_de_cactus/Assets/Scripts/Helpers/Spring.cs
@@ -7,13 +7,15 @@ public class Spring : MonoBehaviour
     public PlayerController playerControl;
     [SerializeField] private float springForce;
     // Start is called before the first frame update
-    void Start(){}
+    void Start() { }
 
     // Update is called once per frame
-    void Update(){}
+    void Update() { }
 
-    private void OnTriggerEnter(Collider other){
-        if (other.tag=="PlayerAttack"){
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerAttack")
+        {
             //Debug.Log("RESORTE: Salta Player");
             playerControl.Bounce(springForce);
         }
