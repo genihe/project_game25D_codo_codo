538ce94b9a8f4a6cdf7ec41753aab174390c8be7
genihe <genihe@hotmail.com>
2023-07-06 17:21:05 -0300

    Se preparo la escena de el enfrentamiento con el Boss

--- el_escape_de_cactus/Assets/Scripts/CameraFollow.cs
+++ el_escape_de_cactus/Assets/Scripts/CameraFollow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 	public Transform target;
+	public Transform target2;
 	public bool lookAtPlayer;
 	public float smothing = 6f;
 	Vector3 offset;
@@ -23,6 +24,8 @@ public class CameraFollow : MonoBehaviour
 		if (lookAtPlayer){
 			Vector3 targetCamPosition = target.position + offset;
 			transform.position = Vector3.Lerp(transform.position , targetCamPosition, smothing * Time.deltaTime);  
+		}else{
+			transform.position = Vector3.Lerp(transform.position , new Vector3(-1.05f,0.95f,0.27f), smothing * Time.deltaTime);  
 		}
 	}
 }
