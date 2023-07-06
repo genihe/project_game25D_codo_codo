1671ca65d5242a3e19de8550309e24bf99049c11
genihe <genihe@hotmail.com>
2023-07-06 14:24:11 -0300

    Se edito la camara

--- el_escape_de_cactus/Assets/Scripts/CameraFollow.cs
+++ el_escape_de_cactus/Assets/Scripts/CameraFollow.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 	public Transform target;
+	public bool lookAtPlayer;
 	public float smothing = 6f;
 	Vector3 offset;
 	
 	private void Start(){
+		lookAtPlayer=true;
 		offset = transform.position - target.position;
 		Debug.Log(offset);
 	}
 	private void FixedUpdate()
 	{
-		Vector3 targetCamPosition = target.position + offset;
-		transform.position = Vector3.Lerp(transform.position , targetCamPosition, smothing * Time.deltaTime);  
+		LookAtPlayer();
+	}
+
+	void LookAtPlayer(){
+		if (lookAtPlayer){
+			Vector3 targetCamPosition = target.position + offset;
+			transform.position = Vector3.Lerp(transform.position , targetCamPosition, smothing * Time.deltaTime);  
+		}
 	}
 }
